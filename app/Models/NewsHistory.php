047d194088c7c97feb39e15a67eb7f5ee9fb49d8
RONIEJISA <roniejisa@gmail.com>
2021-09-09 12:35:34 +0700

    update code

--- app/Models/NewsHistory.php
+++ app/Models/NewsHistory.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use vanhenry\manager\model\HUser;
 class NewsHistory extends Model
-{
+{   
+    protected $fillable = ['content_old','content','news_id','created_at','updated_at','type','h_user_id'];
+    
     use HasFactory;
 
     public function news(){
